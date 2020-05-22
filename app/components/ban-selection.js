7b22196af42d4d1e27f02b510d9e20463c7b81dc
Lucas <lucas.overland@gmail.com>
2020-05-22 18:17:04 -0500

    fix submit ban

--- app/components/ban-selection.js
+++ app/components/ban-selection.js
@@ -2,7 +2,7 @@ import Component from '@glimmer/component';
 import { computed } from '@ember/object';
 
 export default class Lobby extends Component {
-  @computed('args{opponentDecks.@each.isBanned,numberOfBans}')
+  @computed('args.{opponentDecks.@each.isBanned,numberOfBans}')
   get validBansChosen() {
     return (
       this.args.opponentDecks.filter(val => val.isBanned).length ===
